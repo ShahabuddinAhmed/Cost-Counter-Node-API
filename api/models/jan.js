8e8d4b6e0b054fab6c4cdb69a8fe09c845d78a34
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-06 22:38:42 +0600

    jan route is created

--- api/models/jan.js
+++ api/models/jan.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-const usersSchema = mongoose.Schema({
+const janSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     date: { type: String, required: true },
     month: { type: String, required: true },
     year: { type: String, required: true },
-    itemName: { type: String, required },
-    cost: { type: Integer, required },
+    itemName: { type: String, required: true },
+    cost: { type: Integer, required: true },
     users_id: { type: String, required: true, unique: true }
 });
