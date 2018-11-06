df01f6b0d74d08f1aa8e6abe6b4ba00b9b49d9f2
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-06 23:58:32 +0600

    nov route is created

---
+++ api/models/nov.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const novSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    date: { type: String, required: true },
+    month: { type: String, required: true },
+    year: { type: String, required: true },
+    itemName: { type: String, required: true },
+    cost: { type: Integer, required: true },
+    users_id: { type: String, required: true, unique: true }
+});
