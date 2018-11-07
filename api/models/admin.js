bc69e14b740e37319569ea6723e13374caed3e5e
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-08 00:46:11 +0600

    admin model is created

---
+++ api/models/admin.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const adminsSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
