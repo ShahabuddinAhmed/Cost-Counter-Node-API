d7bdd606e7676c9670174f3b5c342cb5b2c67258
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-08 22:04:53 +0600

    feb model is added

--- api/models/feb.js
+++ api/models/feb.js
@@ -7,5 +7,7 @@ const febSchema = mongoose.Schema({
     year: { type: String, required: true },
     itemName: { type: String, required: true },
     cost: { type: Integer, required: true },
-    users_id: { type: String, required: true, unique: true }
+    users_id: { type: mongoose.Schema.Types.ObjectId, ref: "Users" }
 });
+
+module.exports = mongoose.model("Feb", febSchema);
