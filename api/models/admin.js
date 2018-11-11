ebb3652c5d664a313e668a2c973110bd0ca66279
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-11 22:05:27 +0600

    admin model is modified

--- api/models/admin.js
+++ api/models/admin.js
@@ -7,3 +7,5 @@ const adminsSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
+
+module.exports = mongoose.model('Admin', adminsSchema);
