2aacbac5a2f4074d881c3bf100f82f34702ed392
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-14 20:41:50 +0600

    oct model is added

---
+++ api/models/oct.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const octSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    itemName: { type: String, required: true },
+    cost: { type: Number, required: true },
+    date: { type: String, required: true },
+    month: { type: String, required: true },
+    year: { type: String, required: true },
+    users_id: { type: mongoose.Schema.Types.ObjectId, ref: "Users" }
+});
+
+module.exports = mongoose.model("Oct", octSchema);
