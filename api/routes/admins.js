daa954eca89d43192f48e5f86a62bcf8a4363342
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-14 17:36:25 +0600

    POST GET PATCH & DELETE operation is added

---
+++ api/routes/admins.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Admin = require('../models/admin');
+
+router.get('/', (req, res, next) => {
+    Admin.find()
+    .exec()
+    .then(docs => {
+        if(docs > 0) {
+            console.log("From database", docs);
+            res.status(200).json({
+                message: "Data is successfully gotten",
+                docs
+            });
+        } else {
+            res.status(404).json({
+                message: "Users document is empty."
+            });
+        }
+    })
+    .catch(err => {
+        res.status(200).json({
+            message: err
+        });
+    })
+});
+
+router.post('/', (req, res, next) => {
+    const admin = new Admin({
+        _id: new mongoose.Types.ObjectId(),
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password
+    });
+    admin
+    .save()
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => console.log(err));
+    res.status(201).json({
+        message: 'Data successfully is added',
+        userInfo: admin
+    });
+});
+
+router.get('/:userID', (req, res, next) => {
+    const id = req.params.userID;
+    Admin.findById(id)
+    .exec()
+    .then(doc => {
+        if(doc) {
+            console.log("From database", doc);
+            res.status(200).json({
+                message: "Data is successfully gotten by provided ID",
+                doc
+            });
+        } else {
+            res.status(404).json({
+                message: "No data is found by provided ID"
+            });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    })
+});
+
+router.patch('/:userID', (req, res, next) => {
+    const id = req.params.userID;
+    const updateOps = {};
+    for(const ops of req.body) {
+        updateOps[ops.updateName] = ops.value;
+    }
+    Admin.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json({
+            message: "Data is successfully updated",
+            result
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+router.delete('/:userID', (req, res, next) => {
+    const id = req.params.userID;
+    Admin.remove({ _id: id })
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            message: "Data is successfully deleted",
+            result
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    })
+});
+
+module.exports = router;
