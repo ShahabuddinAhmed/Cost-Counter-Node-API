978d99969cca75410f4cd938edb8f5f31df5bb9a
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-15 18:20:28 +0600

    some new roue is added

--- app.js
+++ app.js
@@ -5,7 +5,19 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const userRoutes = require('./api/routes/users');
+const adminRoutes = require('./api/routes/admins');
+const janRoutes = require('./api/routes/jan');
+const febRoutes = require('./api/routes/feb');
+const marRoutes = require('./api/routes/mar');
 const aprRoutes = require('./api/routes/apr');
+const mayRoutes = require('./api/routes/may');
+const junRoutes = require('./api/routes/jun');
+const julRoutes = require('./api/routes/jul');
+const augRoutes = require('./api/routes/aug');
+const sepRoutes = require('./api/routes/sep');
+const octRoutes = require('./api/routes/oct');
+const novRoutes = require('./api/routes/nov');
+const decRoutes = require('./api/routes/dec');
 
 mongoose.connect('mongodb://cost-counter:cost-counter@database-shard-00-00-v0ueh.gcp.mongodb.net:27017,database-shard-00-01-v0ueh.gcp.mongodb.net:27017,database-shard-00-02-v0ueh.gcp.mongodb.net:27017/house-renting?ssl=true&replicaSet=database-shard-0&authSource=admin&retryWrites=true', {
     useNewUrlParser: true
@@ -33,7 +45,19 @@ app.use((req, res, next) => {
 
 // Routes which should handle request
 app.use('/user', userRoutes);
+app.use('/admin', adminRoutes);
+app.use('/jan', janRoutes);
+app.use('/feb', febRoutes);
+app.use('/mar', marRoutes);
 app.use('/apr', aprRoutes);
+app.use('/may', mayRoutes);
+app.use('/jun', junRoutes);
+app.use('/jul', julRoutes);
+app.use('/aug', augRoutes);
+app.use('/sep', sepRoutes);
+app.use('/oct', octRoutes);
+app.use('/nov', novRoutes);
+app.use('/dec', decRoutes);
 
 // This meddileware Handle every http request error
 // This error means could not find any route
