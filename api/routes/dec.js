e38f399dbbf61c2fc6e1090fafe98c517efe691f
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-21 02:58:07 +0600

    route validation check is moved to controller

--- api/routes/dec.js
+++ api/routes/dec.js
@@ -1,117 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const Dec = require('../models/dec');
+const checkAuth = require('../middleware/check-auth');
+const decController = require('../controller/dec');
 
-router.get('/', (req, res, next) => {
-    Dec.find()
-    .exec()
-    .then(docs => {
-        if(docs > 0) {
-            console.log("From database", docs);
-            res.status(200).json({
-                message: "Data is successfully gotten",
-                docs
-            });
-        } else {
-            res.status(404).json({
-                message: "Users document is empty."
-            });
-        }
-    })
-    .catch(err => {
-        res.status(200).json({
-            message: err
-        });
-    })
-});
-
-router.post('/', (req, res, next) => {
-    const dec = new Dec({
-        _id: new mongoose.Types.ObjectId(),
-        itemName: req.body.itemName,
-        cost: req.body.cost,
-        date: req.body.date,
-        month: req.body.month,
-        year: req.body.year,
-        users_id: req.body.users_id
-    });
-    dec
-    .save()
-    .then(result => {
-        console.log(result);
-    })
-    .catch(err => console.log(err));
-    res.status(201).json({
-        message: 'Data successfully is added',
-        userInfo: dec
-    });
-});
-
-router.get('/:userID', (req, res, next) => {
-    const id = req.params.userID;
-    Dec.find({users_id: id})
-    .exec()
-    .then(doc => {
-        if(doc) {
-            console.log("From database", doc);
-            res.status(200).json({
-                message: "Data is successfully gotten by provided ID",
-                doc
-            });
-        } else {
-            res.status(404).json({
-                message: "No data is found by provided ID"
-            });
-        }
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    })
-});
-
-router.patch('/:userID', (req, res, next) => {
-    const id = req.params.userID;
-    const updateOps = {};
-    for(const ops of req.body) {
-        updateOps[ops.updateName] = ops.value;
-    }
-    Dec.update({ _id: id }, { $set: updateOps })
-    .exec()
-    .then(result => {
-        console.log(result);
-        res.status(200).json({
-            message: "Data is successfully updated",
-            result
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
-
-router.delete('/:userID', (req, res, next) => {
-    const id = req.params.userID;
-    Dec.remove({ _id: id })
-    .exec()
-    .then(result => {
-        res.status(200).json({
-            message: "Data is successfully deleted",
-            result
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    })
-});
+router.get('/', checkAuth, decController.dec_get_all);
+router.post('/', checkAuth, decController.dec_post);
+router.get('/:userID', checkAuth, decController.dec_get_one);
+router.patch('/:userID', checkAuth, decController.dec_update);
+router.delete('/:userID', checkAuth, decController.dec_delete);
 
 module.exports = router;
