0230c14ed4c5cfc582d1d5d670a9b7e1789663d3
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-21 02:57:01 +0600

    route validation check is moved to controller

--- api/routes/apr.js
+++ api/routes/apr.js
@@ -1,122 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const Apr = require('../models/apr');
+const checkAuth = require('../middleware/check-auth');
+const aprController = require('../controller/apr');
 
-router.get('/', (req, res, next) => {
-    Apr.find()
-    .exec()
-    .then(docs => {
-        if(docs.length >=1) {
-            console.log("From database", docs);
-            res.status(200).json({
-                message: "Data is successfully gotten",
-                docs
-            });
-        } else {
-            res.status(404).json({
-                message: "Users document is empty."
-            });
-        }
-    })
-    .catch(err => {
-        res.status(200).json({
-            message: err
-        });
-    })
-});
-
-router.post('/', (req, res, next) => {
-    const apr = new Apr({
-        _id: new mongoose.Types.ObjectId(),
-        itemName: req.body.itemName,
-        cost: req.body.cost,
-        date: req.body.date,
-        month: req.body.month,
-        year: req.body.year,
-        users_id: req.body.users_id
-    });
-    apr
-    .save()
-    .then(result => {
-        console.log(result);
-        res.status(201).json({
-            message: 'Data successfully is added',
-            userInfo: apr
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(400).json({
-            error: err
-        });
-    });
-});
-
-router.get('/:userID', (req, res, next) => {
-    const id = req.params.userID;
-    Apr.find({users_id: id})
-    .exec()
-    .then(doc => {
-        if(doc.length >=1) {
-            console.log("From database", doc);
-            res.status(200).json({
-                message: "Data is successfully gotten by provided ID",
-                doc
-            });
-        } else {
-            res.status(404).json({
-                message: "No data is found by provided ID"
-            });
-        }
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    })
-});
-
-router.patch('/:userID', (req, res, next) => {
-    const id = req.params.userID;
-    const updateOps = {};
-    for(const ops of req.body) {
-        updateOps[ops.updateName] = ops.value;
-    }
-    Apr.update({ _id: id }, { $set: updateOps })
-    .exec()
-    .then(result => {
-        console.log(result);
-        res.status(200).json({
-            message: "Data is successfully updated",
-            result
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
-
-router.delete('/:userID', (req, res, next) => {
-    const id = req.params.userID;
-    Apr.remove({ _id: id })
-    .exec()
-    .then(result => {
-        res.status(200).json({
-            message: "Data is successfully deleted",
-            result
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    })
-});
+router.get('/', checkAuth, aprController.apr_get_all);
+router.post('/', checkAuth, aprController.apr_post);
+router.get('/:userID', checkAuth, aprController.apr_get_one);
+router.patch('/:userID', checkAuth, aprController.apr_update);
+router.delete('/:userID', checkAuth, aprController.apr_delete);
 
 module.exports = router;
