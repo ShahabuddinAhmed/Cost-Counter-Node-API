7fc094dc0eaebe4f0974dd15bb7a1562d46b857f
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-21 11:33:50 +0600

    route validation check is moved to controller

--- api/routes/admins.js
+++ api/routes/admins.js
@@ -1,115 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const Admin = require('../models/admin');
+const checkAuth = require('../middleware/admin-auth');
+const adminController = require('../controller/admin');
 
-router.get('/', (req, res, next) => {
-    Admin.find()
-    .exec()
-    .then(docs => {
-        if(docs > 0) {
-            console.log("From database", docs);
-            res.status(200).json({
-                message: "Data is successfully gotten",
-                docs
-            });
-        } else {
-            res.status(404).json({
-                message: "Users document is empty."
-            });
-        }
-    })
-    .catch(err => {
-        res.status(200).json({
-            message: err
-        });
-    })
-});
-
-router.post('/', (req, res, next) => {
-    const admin = new Admin({
-        _id: new mongoose.Types.ObjectId(),
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password
-    });
-    admin
-    .save()
-    .then(result => {
-        console.log(result);
-    })
-    .catch(err => console.log(err));
-    res.status(201).json({
-        message: 'Data successfully is added',
-        userInfo: admin
-    });
-});
-
-router.get('/:userID', (req, res, next) => {
-    const id = req.params.userID;
-    Admin.findById(id)
-    .exec()
-    .then(doc => {
-        if(doc) {
-            console.log("From database", doc);
-            res.status(200).json({
-                message: "Data is successfully gotten by provided ID",
-                doc
-            });
-        } else {
-            res.status(404).json({
-                message: "No data is found by provided ID"
-            });
-        }
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    })
-});
-
-router.patch('/:userID', (req, res, next) => {
-    const id = req.params.userID;
-    const updateOps = {};
-    for(const ops of req.body) {
-        updateOps[ops.updateName] = ops.value;
-    }
-    Admin.update({ _id: id }, { $set: updateOps })
-    .exec()
-    .then(result => {
-        console.log(result);
-        res.status(200).json({
-            message: "Data is successfully updated",
-            result
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
-
-router.delete('/:userID', (req, res, next) => {
-    const id = req.params.userID;
-    Admin.remove({ _id: id })
-    .exec()
-    .then(result => {
-        res.status(200).json({
-            message: "Data is successfully deleted",
-            result
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    })
-});
+router.get('/', checkAuth, adminController.getAllAdmin);
+router.post('/register',checkAuth, adminController.createAdmin);
+router.post('/login', adminController.adminLogin);
+router.get('/:userID', checkAuth, adminController.getOneAdmin);
+router.patch('/:userID', checkAuth, adminController.updateAdmin);
+router.delete('/:userID', checkAuth, adminController.deleteAdmin);
 
 module.exports = router;
