4384c7e676c0429935d65db59dbb93f8aba97147
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-21 11:33:11 +0600

    email varificatrion is added

--- api/models/users.js
+++ api/models/users.js
@@ -4,7 +4,12 @@ const usersSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     firstName: { type: String, required: true, unique: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
     address: { type: String, required:true },
     password: { type: String, required: true }
 });
