903ab9e0fc61c4f65d03e5de7b5f457ab9ae99a1
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-25 18:09:50 +0600

    user is updated

--- api/models/users.js
+++ api/models/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const usersSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    firstName: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: {
         type: String,
